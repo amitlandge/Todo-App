91c3d2092c4c93033b8beaa0f7e8089a87714a2e
Amit <amitlandge333@gmail.com>
2022-12-19 10:34:46 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+let input = document.querySelector(".input");
+let addBtn = document.querySelector(".addButton");
+let container = document.querySelector(".container");
+
+addBtn.addEventListener("click", () => {
+  let task = input.value;
+  // create div element 
+  let createEle = document.createElement("div");
+  createEle.classList.add("item");
+  // create input element 
+  let createInput = document.createElement("input");
+  createInput.classList.add("item_input");
+  (createInput.type = "text"), createInput.setAttribute("readonly", "readonly");
+  createInput.value = task;
+  createEle.appendChild(createInput);
+  container.appendChild(createEle);
+  // create edit button 
+  let createEdit_button = document.createElement("button");
+  createEdit_button.innerHTML = "EDIT";
+  createEdit_button.classList.add("editButton");
+  // create delete Button 
+  let createDelete_Button = document.createElement("button");
+  createDelete_Button.innerHTML = "DELETE";
+  createDelete_Button.classList.add("deleteButton");
+  createEle.appendChild(createEdit_button);
+  createEle.appendChild(createDelete_Button);
+  createEdit_button.addEventListener("click", () => {
+    console.log("edit");
+    if (createEdit_button.innerText.toLowerCase() == "edit") {
+      createEdit_button.innerText = "Save";
+      createInput.removeAttribute("readonly");
+      createInput.focus();
+    } else {
+      createEdit_button.innerText = "EDIT";
+      createEdit_button.setAttribute("readonly", "readonly");
+    }
+  });
+  createDelete_Button.addEventListener("click", () => {
+    createDelete_Button.parentElement.remove();
+  });
+});
