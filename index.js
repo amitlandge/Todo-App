b6a4232e99a6149457586bed0528f1d5bb90ae3f
Amit <amitlandge333@gmail.com>
2022-12-19 12:36:59 +0530

    validation

--- index.js
+++ index.js
@@ -4,21 +4,26 @@ let container = document.querySelector(".container");
 
 addBtn.addEventListener("click", () => {
   let task = input.value;
-  // create div element 
+  if (!task) {
+    return;
+  } else if (task.trim().length < 1) {
+    return;
+  }
+  // create div element
   let createEle = document.createElement("div");
   createEle.classList.add("item");
-  // create input element 
+  // create input element
   let createInput = document.createElement("input");
   createInput.classList.add("item_input");
   (createInput.type = "text"), createInput.setAttribute("readonly", "readonly");
   createInput.value = task;
   createEle.appendChild(createInput);
   container.appendChild(createEle);
-  // create edit button 
+  // create edit button
   let createEdit_button = document.createElement("button");
   createEdit_button.innerHTML = "EDIT";
   createEdit_button.classList.add("editButton");
-  // create delete Button 
+  // create delete Button
   let createDelete_Button = document.createElement("button");
   createDelete_Button.innerHTML = "DELETE";
   createDelete_Button.classList.add("deleteButton");
